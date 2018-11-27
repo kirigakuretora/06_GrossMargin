f706d39b6f70b428c6c208880f0a940209084aef
kirigakuretora <fe21st@yahoo.com.tw>
2018-11-27 17:00:24 +0800

    Add Helps

--- WpfApp.Library/Models/Accountant.cs
+++ WpfApp.Library/Models/Accountant.cs
@@ -2,11 +2,17 @@
 
 namespace WpfApp.Library.Models
 {
-    public class Accountant
+    public class Accountant:WpfApp.Library.Models.Discrimination.NetAndAfterTax
     {
+        #region 0.Global Variable
+        internal new Boolean IsTrue() { return true; }
+        #endregion
+
+        #region 1.Load
         public Decimal GrossMargin(Decimal Revenue, Decimal CGOS, Decimal Percent)
         {
             return Revenue / (Percent - CGOS / 100);
         }
+        #endregion
     }
 }
---
+++ WpfApp.Library/Models/Discrimination/NetAndAfterTax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Library.Models.Discrimination
+{
+   public class NetAndAfterTax
+    {
+        /// <summary>
+        /// 識別稅前與稅後
+        /// </summary>
+        /// <returns></returns>
+        public Boolean IsTrue() { return false; }
+    }
+}
