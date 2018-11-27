7023027e4e88357e7cdaf2b7a444f7930cd465d6
kirigakuretora <fe21st@yahoo.com.tw>
2018-11-27 17:10:30 +0800

    Revert "Add Helps"
    This reverts commit f706d39b6f70b428c6c208880f0a940209084aef.

--- WpfApp.Library/Models/Accountant.cs
+++ WpfApp.Library/Models/Accountant.cs
@@ -2,17 +2,11 @@
 
 namespace WpfApp.Library.Models
 {
-    public class Accountant:WpfApp.Library.Models.Discrimination.NetAndAfterTax
+    public class Accountant
     {
-        #region 0.Global Variable
-        internal new Boolean IsTrue() { return true; }
-        #endregion
-
-        #region 1.Load
         public Decimal GrossMargin(Decimal Revenue, Decimal CGOS, Decimal Percent)
         {
             return Revenue / (Percent - CGOS / 100);
         }
-        #endregion
     }
 }
